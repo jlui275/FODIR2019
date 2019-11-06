f4525dd3ce3b12592200269612cf4a85bb706b0d
jlui275 <justin_lui@baylor.edu>
2019-11-06 02:19:35 -0600

    Monitor Core Temperature Rpi Script

---
+++ monitor_temp.py
@@ -0,0 +1,13 @@
+import os
+
+def measure_temp():
+        temp = os.popen("vcgencmd measure_temp").readline()
+        return (temp.replace("temp=",""))
+
+def convert_fahrenheit(temp):
+    temp = temp[:3]
+    fahrenheit = (double(temp) * (9/5)) + 32
+    return fahrenheit
+
+print(measure_temp())
+print "%s'F" %convert_fahrenheit(measure_temp())
