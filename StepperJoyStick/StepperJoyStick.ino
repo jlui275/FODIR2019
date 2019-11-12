734c356f14eff9fa56643c8b37fa2bb1f93874b7
JoePreviti <35278054+JoePreviti@users.noreply.github.com>
2019-11-12 00:06:59 -0600

    Add files via upload

--- StepperJoyStick/StepperJoyStick.ino
+++ StepperJoyStick/StepperJoyStick.ino
@@ -7,7 +7,10 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <Servo.h>
 
+Servo myservo1;
+Servo myservo2;
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
@@ -17,12 +20,13 @@ Adafruit_StepperMotor *Motor2 = AFMS.getStepper(200, 2);
 
 int stickY = A0;
 int stickX = A1;
-
-
+int angle = 90;
+int altitude = 180;
 void setup()
 {
 
-
+  myservo1.attach(10);
+  myservo2.attach(9);
   //further research necessary
   //myStepper.setSpeed(10);//RPM
 //    myStepper.setSpeed(10);
@@ -30,56 +34,66 @@ void setup()
     AFMS.begin();
     Motor1->setSpeed(30);
     Motor2->setSpeed(30);
+    myservo1.write(90);
+    myservo2.write(180);
+    
 }
 
 void loop()
 {
-//
-//  Motor1->step(100, FORWARD, SINGLE); 
-//  delay(500);
-//  Motor1->step(100, BACKWARD, SINGLE); 
-//  delay(500);
+//    Motor1->step(1, BACKWARD, SINGLE); 
+//    Motor2->step(1, FORWARD, SINGLE); 
+//if(angle == 180){
+//    angle = 0;
+//  }
 
-
+  
+//  angle++;
+  
   //FORWARD MOVEMENT
   if (analogRead(stickY) < 300  & analogRead(stickX) > 300 & analogRead(stickX) < 700  ) {
-
-    Motor2->step(1, FORWARD, SINGLE); 
-    Motor2->step(1, BACKWARD, SINGLE); 
-    delay(30);
+    //Motor1->step(1, FORWARD, SINGLE); 
+//    Motor2->step(1, FORWARD, SINGLE); 
+    altitude+=2;
+    myservo2.write(altitude);
+    Serial.print("FORWARD");
+    Serial.print("\n");
   }
 
   //BACKWARD MOVEMENT
   if (analogRead(stickY) > 700  & analogRead(stickX) > 300 & analogRead(stickX) < 700  ) {
-    Motor2->step(1, BACKWARD, SINGLE); 
-    Motor2->step(1, FORWARD, SINGLE); 
-    delay(30);
+    Serial.print("BACKWARD");
+    Serial.print("\n");
+    altitude-=2;
+    myservo2.write(altitude);
 
 
   }
 
-  //TURN LEFT
-  if (analogRead(stickY) > 700  & analogRead(stickX) > 300 & analogRead(stickX) < 700  ) {
-    Motor2->step(1, BACKWARD, SINGLE); 
-    Motor2->step(1, FORWARD, SINGLE); 
-    delay(30);
-
+//  //TURN LEFT
+  if (analogRead(stickY) < 700  & analogRead(stickY) > 300 & analogRead(stickX) < 300  ) {
+    Serial.print("LEFT");
+    Serial.print("\n");
+    angle+=2;
+    myservo1.write(angle);
 
   }
 
   //TURN RIGHT
-  if (analogRead(stickY) > 700  & analogRead(stickX) > 300 & analogRead(stickX) < 700  ) {
-    Motor2->step(1, BACKWARD, SINGLE); 
-    Motor2->step(1, FORWARD, SINGLE); 
-    delay(30);
-
-
-  }
-  
-  else {
-
-    Serial.print("NEUTRAL");
+  if (analogRead(stickY) < 700  & analogRead(stickY) > 300 & analogRead(stickX) > 700  ) {
+    Serial.print("RIGHT");
     Serial.print("\n");
+    angle-=2;
+    myservo1.write(angle);
+    
+
   }
-//delay(500); //may comment out and use setSpeed to a low RPM
+//  IN NEUTRAL POSITION
+//  if (analogRead(stickY) < 700  & analogRead(stickY) > 300 & analogRead(stickX) < 700 & analogRead(stickX) > 300  ) {
+//    Motor1->release();
+//    Serial.print("NEUTRAL");
+//    Serial.print("\n");
+//  }
+  
+delay(75); 
 }
